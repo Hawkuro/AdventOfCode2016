391fb9f70bf9fdc136f227a8fd8f43bdea8ce9a1
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-12-03 12:27:43 +0000

    Day 3 complete

---
+++ AdventOfCode2016/Day 3/D3Prob1.fsx
@@ -0,0 +1,13 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D3input.txt")
+
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = int 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let parseTriangle (str : string) =
+    match str.Split([|' '|],System.StringSplitOptions.RemoveEmptyEntries) with
+    | [|Integer a; Integer b; Integer c|] -> [a;b;c]
+    | _ -> failwith ("failed to parse string: "+str)
+
+let parsedInput = input |> Seq.map parseTriangle |> Seq.map List.sort |> Seq.filter (fun [a;b;c] -> a+b > c) |> Seq.length |> printfn "%A"
---
+++ AdventOfCode2016/Day 3/D3Prob2.fsx
@@ -0,0 +1,26 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D3input.txt")
+
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = int 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let parseTriangle (str : string) =
+    match str.Split([|' '|],System.StringSplitOptions.RemoveEmptyEntries) with
+    | [|Integer a; Integer b; Integer c|] -> [a;b;c]
+    | _ -> failwith ("failed to parse string: "+str)
+
+let parsedInput = input |> Seq.map parseTriangle |> Seq.toList
+
+let flipTriplets triangleList =
+    let rec flipTriplets' triangleList out =
+        match triangleList with
+        | [] -> out
+        // a1 b1 c1    a1 a2 a3
+        // a2 b2 c3 -> b1 b2 b3
+        // a3 b3 c3    c1 c2 c3
+        | [a1;b1;c1]::[a2;b2;c2]::[a3;b3;c3]::tail -> flipTriplets' tail ([a1;a2;a3]::[b1;b2;b3]::[c1;c2;c3]::out)
+        | _ -> failwith ""
+    flipTriplets' triangleList []
+
+parsedInput |> flipTriplets |> Seq.map List.sort |> Seq.filter (fun [a;b;c] -> a+b > c) |> Seq.length |> printfn "%A"
