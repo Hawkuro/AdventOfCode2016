99c0746dbeb8a77992114f3d4d07567446433032
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-12-06 23:32:32 +0000

    Added Day 6

---
+++ AdventOfCode2016/Day 6/D6Prob2.fsx
@@ -0,0 +1,10 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D6input.txt")
+
+let rec transpose = function
+    | (_::_)::_ as M -> List.map List.head M :: transpose (List.map List.tail M)
+    | _ -> []
+
+let charsToString cs =
+    new string(Seq.toArray cs)
+
+input |> Array.map Seq.toList |> Array.toList |> transpose |> List.map (List.countBy id >> List.minBy snd) |> List.map fst |> charsToString |> printfn "%A"
