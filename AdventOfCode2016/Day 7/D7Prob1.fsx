f5789ef719de45823008d6ec5c039f4fabc29afe
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-12-07 22:32:58 +0000

    Day 7 prob 1 done, 2 started

---
+++ AdventOfCode2016/Day 7/D7Prob1.fsx
@@ -0,0 +1,22 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D7input.txt")
+
+open System.Text.RegularExpressions
+
+let hypernetRegex = new Regex("\[[^\]]*\]")
+let matchedStrings (r:Regex) (s : string) = r.Matches s |> Seq.cast |> Seq.map (fun m -> m.ToString())
+
+let hasABBA (str:string) =
+    let rec hasABBA' cList =
+        match cList with
+        | a::b::c::d::tail when a <> b && a = d && b = c -> true
+        | a::b::c::d::[] -> false
+        | head::tail -> hasABBA' tail
+        | _ -> false
+    hasABBA' (str |> Seq.toList)
+
+let isStringValid (str:string) =
+    (hypernetRegex.Split str,matchedStrings hypernetRegex str |> Seq.map (fun (s:string) -> s.Substring(1,s.Length-2)))
+    |> (fun (a,b) -> (a |> Seq.exists hasABBA,b |> Seq.exists hasABBA))
+    |> (=) (true,false)
+
+input |> Seq.filter isStringValid |> Seq.length |> printfn "%A"
---
+++ AdventOfCode2016/Day 7/D7Prob2.fsx
@@ -0,0 +1,22 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D7input.txt")
+
+open System.Text.RegularExpressions
+
+let hypernetRegex = new Regex("\[[^\]]*\]")
+let matchedStrings (r:Regex) (s : string) = r.Matches s |> Seq.cast |> Seq.map (fun m -> m.ToString())
+
+let hasABA (str:string) =
+    let rec hasABA' cList =
+        match cList with
+        | a::b::c::tail when a <> b && a = c -> true
+        | a::b::c::[] -> false
+        | head::tail -> hasABA' tail
+        | _ -> false
+    hasABA' (str |> Seq.toList)
+
+let isStringValid (str:string) =
+    (hypernetRegex.Split str,matchedStrings hypernetRegex str |> Seq.map (fun (s:string) -> s.Substring(1,s.Length-2)))
+    |> (fun (a,b) -> (a |> Seq.exists hasABA,b |> Seq.exists hasABA))
+    |> (=) (true,false)
+
+input |> Seq.filter isStringValid |> Seq.length |> printfn "%A"
